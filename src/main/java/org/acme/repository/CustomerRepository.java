b4d03501b6e1c15919897c3d9b95c5997199e3da
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-21 11:22:04 +0300

    Add Logs

--- src/main/java/org/acme/repository/CustomerRepository.java
+++ src/main/java/org/acme/repository/CustomerRepository.java
@@ -1,30 +1,28 @@
-
 package org.acme.repository;
 
-import static io.quarkus.arc.ComponentsProvider.LOG;
 import org.acme.entity.Customer;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepository; //use PanacheRepository from Quarkus
+import io.quarkus.logging.Log; //use Quarkus logging
+
 import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class CustomerRepository implements PanacheRepository<Customer> {
-    
-     public Optional<Customer> findByVat(String vat) {
+
+    public Optional<Customer> findByVat(String vat) {
         try {
             Optional<Customer> customer = find("vat", vat).firstResultOptional();
 
             if (customer.isPresent()) {
-                LOG.info(String.format("Customer retrieved successfully with VAT: %s", vat));
+                Log.infof("Customer retrieved successfully with VAT: %s", vat);
             } else {
-                LOG.warn(String.format("No customer found with VAT: %s", vat));
+                Log.warnf("No customer found with VAT: %s", vat);
             }
             return customer;
         } catch (Exception e) {
-            LOG.error(String.format("Error retrieving customer with VAT: %s - %s", vat, e.getMessage()), e);
+            Log.errorf(e, "Error retrieving customer with VAT: %s", vat);
             return Optional.empty();
         }
     }
 }
-
-   

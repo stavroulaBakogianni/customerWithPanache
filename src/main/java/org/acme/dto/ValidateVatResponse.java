2e85160e3f2b4d32faf9c1692d13c92dc3f76899
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-21 11:20:24 +0300

    Create ValidateVatResponse.java

---
+++ src/main/java/org/acme/dto/ValidateVatResponse.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.acme.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author stavroulabakogianni
+ */
+@Getter
+@NoArgsConstructor
+public class ValidateVatResponse {
+        private boolean valid;
+}
